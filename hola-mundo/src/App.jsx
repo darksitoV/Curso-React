e6f4ec101bd5b5b67d0b1a256f761d50a912b2a1
Genaro Isaac Cardenas Soto <mx90367910@coppel.com>
2025-10-25 18:12:31 -0700

    Avance inicio de la practica followCard

---
+++ hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+  return (
+    <article>
+      <header></header>
+      <img alt="El avatar de darksito" src="https://avatars.githubusercontent.com/u/183795272?v=4"/>
+      <div>
+      <strong>Isaac Cardenas</strong>
+      <span>@darksitoV</span>      
+      </div>
+    <aside>
+      <button>
+        seguir
+      </button>
+    </aside>
+    </article>
+  )
+}
+
+// export default App;
